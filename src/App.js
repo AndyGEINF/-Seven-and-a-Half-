aaf6b851f500b4f19fa2703141c1711dd419c8b0
Andy <u1987276@campus.udg.edu>
2024-10-24 22:01:43 +0200

    fondo de pantalla

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Carta from './components/Carta/Carta.js'
+import './App.css';
 
 const Container = styled.div`
   padding:20px;
 `;
 const App = () => {
   return (
+    <>
+    <div className="pantalla-inicial">
+      <h1>SEVEN & HALF</h1>
+      <h2>¡Ganar dinero nunca fue tan fácil!</h2>
+      <button>Comenzar</button>
+    </div>
     <Container>
       <Carta palo="trebol" numero="J">
 
       </Carta>
     </Container>
+    </>
   );
 };
 
