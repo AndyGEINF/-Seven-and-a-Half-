9280707b3da60a76131a08a8ddb9a56062345c2a
Andy <u1987276@campus.udg.edu>
2024-10-28 19:48:05 +0100

    path a Login

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+// components/Login.js
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Iniciar Sesión</h2>
+      {/* Aquí puedes agregar tu formulario de inicio de sesión */}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/PantallaInicial.js
@@ -0,0 +1,22 @@
+// components/PantallaInicial.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './PantallaInicial.css'; // Crea un archivo CSS si necesitas estilos específicos
+
+const PantallaInicial = () => {
+  const navigate = useNavigate();
+
+  const handleComenzar = () => {
+    navigate('/login'); // Navega a la ruta de inicio de sesión
+  };
+
+  return (
+    <div className="pantalla-inicial">
+      <h1>SEVEN & HALF</h1>
+      <h2>¡Ganar dinero nunca fue tan fácil!</h2>
+      <button onClick={handleComenzar}>Comenzar</button>
+    </div>
+  );
+};
+
+export default PantallaInicial;
