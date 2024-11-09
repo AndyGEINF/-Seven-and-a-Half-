e966d3fc349893221a91454b6a3688a6554cfce6
mikixsc <u1987334@campus.udg.edu>
2024-11-09 20:14:55 +0100

    restaurar

--- src/Login.js
+++ src/Login.js
@@ -1,237 +1,80 @@
-// components/Login.js
-import React from "react";
-import Baraja from "./components/Baraja";
-import Contador from "./components/Contador";
-import styled from 'styled-components';
-import Button from "./components/Button";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
 import mesaCasino from './assets/images/mesaCasino.png';
 
-const Tablero = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  padding: 20px;
+
+const Input = styled.input`
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 200px;  background-color= red; 
+  margin-right: 10px;
+
+  
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  
+  -moz-appearance: textfield;
 `;
 
-const Movimientos = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  gap: 50px;
-`;
-
-
-const Game = styled.div`
+const Background = styled.div`
   height: 100vh;
+  max-width: 100vw;
   background-image: url(${mesaCasino});
   background-size: cover; 
   background-position: center;
-  display: flex;
-  flex-direction: row;
-`;
-
-const Barajas = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
 `;
 
 const Login = () => {
+  const [name, setName] = useState('');
+  const [money, setMoney] = useState(0);
+  const navigate = useNavigate(); // Hook per navegar a una altra ruta
+
+  const handleLogin = () => {
+    const validMoney = isNaN(money) || money === '' ? 0 : money;
+    const validName = name.trim() === '' ? 'Player1' : name;
+    // Navega a la página de juego enviando name y validMoney como el valor de money
+    navigate('/game', { state: { name: validName, money: validMoney } });
+  };
+
+  const handleFocus = () => {
+    if (money === 0) {
+      setMoney('');
+    }
+  };
+
+  const handleBlur = () => {
+    if (money === '') {
+      setMoney(0);
+    }
+  };
+
   return (
-    <Game>
-      {/*<h2>Iniciar Sesión</h2>*/}
-      <Barajas>
-        <Baraja
-          cartas={[
-            { numero: "A", palo: "corazon" },
-            { numero: "2", palo: "diamante" },
-            { numero: "K", palo: "trebol" },
-          ]}
-        ></Baraja>
-        <Baraja
-          cartas={[
-            { numero: "A", palo: "corazon" },
-            { numero: "2", palo: "diamante" },
-            { numero: "K", palo: "trebol" },
-          ]}
-        ></Baraja>
-      </Barajas>
-      <Tablero>
-        <Contador numero="7,5" showLabel label="Dealer"></Contador>
-        <Contador numero="7,5" showLabel label="Jugador"></Contador>
-        <Movimientos>
-          <Button label="Card"></Button>
-          <Button label="Stop"></Button>
-        </Movimientos>
-      </Tablero>
-      {/* 
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "2", palo: "diamante" },
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "2", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "2", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" }
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "10", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "K", palo: "trebol" }
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "10", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" }
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "10", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "K", palo: "trebol" }
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "10", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "K", palo: "trebol" }
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "10", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "Q", palo: "pica" },
-          { numero: "K", palo: "trebol" }
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "10", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "Q", palo: "pica" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "K", palo: "trebol" }
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "A", palo: "corazon" },
-          { numero: "10", palo: "diamante" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "K", palo: "trebol" },
-          { numero: "7", palo: "pica" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "Q", palo: "pica" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "A", palo: "corazon" },
-          { numero: "K", palo: "trebol" }
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "K", palo: "corazon" },
-          { numero: "K", palo: "trebol" },
-          { numero: "K", palo: "diamante" },
-          { numero: "K", palo: "pica" },
-          { numero: "Q", palo: "corazon" },
-          { numero: "Q", palo: "trebol" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "Q", palo: "pica" },
-          { numero: "J", palo: "corazon" },
-          { numero: "J", palo: "trebol" },
-          { numero: "J", palo: "diamante" },
-          { numero: "J", palo: "pica" },
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "K", palo: "corazon" },
-          { numero: "K", palo: "trebol" },
-          { numero: "K", palo: "diamante" },
-          { numero: "K", palo: "pica" },
-          { numero: "Q", palo: "corazon" },
-          { numero: "Q", palo: "trebol" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "Q", palo: "pica" },
-          { numero: "J", palo: "corazon" },
-          { numero: "J", palo: "trebol" },
-          { numero: "J", palo: "diamante" },
-          { numero: "J", palo: "pica" },
-          { numero: "A", palo: "corazon" },
-        ]}
-      ></Baraja>
-      <Baraja
-        cartas={[
-          { numero: "K", palo: "corazon" },
-          { numero: "K", palo: "trebol" },
-          { numero: "K", palo: "diamante" },
-          { numero: "K", palo: "pica" },
-          { numero: "Q", palo: "corazon" },
-          { numero: "Q", palo: "trebol" },
-          { numero: "Q", palo: "diamante" },
-          { numero: "Q", palo: "pica" },
-          { numero: "J", palo: "corazon" },
-          { numero: "J", palo: "trebol" },
-          { numero: "J", palo: "diamante" },
-          { numero: "J", palo: "pica" },
-          { numero: "A", palo: "corazon" },
-          { numero: "A", palo: "trebol" },
-        ]}
-      ></Baraja>
-      */}
-      
-      {/* Aquí puedes agregar tu formulario de inicio de sesión */}
-    </Game>
-  );
+    <Background>
+      <h2>Login</h2>
+      <Input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        type="number"
+        placeholder="Amount of money"
+        value={money}
+        onFocus={handleFocus} // Limpia el valor al fer click
+        onBlur={handleBlur} // Restaura 0 si està buit
+        onChange={(e) => setMoney(parseFloat(e.target.value))}
+      />
+      <button onClick={handleLogin}>Start the game</button>
+    </Background>
+  );  
 };
 
 export default Login;
