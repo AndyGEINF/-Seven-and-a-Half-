313421f83b3bc20262e954f19e366beb449e612b
Andy <u1987276@campus.udg.edu>
2025-03-29 23:56:01 +0100

    fixing

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode basename="/">
-    <Router>
+  <React.StrictMode>
+    <Router basename="/-Seven-and-a-Half-">
       <App />
     </Router>
   </React.StrictMode>
