590a7cfe4a1ed29ebd456b5dae106e9c43d56f82
Andy <u1987276@campus.udg.edu>
2025-03-29 23:46:04 +0100

    fixing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import PantallaInicial from './PantallaInicial.js'; // Importa PantallaInicial
 import Login from './Login.js'; // Importa Login
 import TableGame from './TableGame.js'; // Importa Game
 
-
-
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PantallaInicial />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/game" element={<TableGame />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<PantallaInicial />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/game" element={<TableGame />} />
+    </Routes>
   );
 };
 
